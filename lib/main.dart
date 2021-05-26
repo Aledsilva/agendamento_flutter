5e086103ecf2e4d7c021cb99a9f72014bdf38dde
AledDev <alexandre.silva@lab360.com.br>
2021-05-26 13:54:29 -0300

    Teste de mascara para campos de texto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:agendamento_flutter/home_screen.dart';
+import 'package:agendamento_flutter/maskTeste.dart';
 import 'package:agendamento_flutter/mercado_pago.dart';
-import 'package:agendamento_flutter/reserve_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -10,7 +10,6 @@ void main(){
   Intl.defaultLocale = 'pt_BR';
   initializeDateFormatting();
   runApp(MaterialApp(
-    home: MercadoPago()
+    home: MaskTeste()
   ));
-
 }
